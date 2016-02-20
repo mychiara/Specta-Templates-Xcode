b5429524bc54e03195c110464ead549fab9f1a1a
Andy Ruck <mychiara@gmail.com>
2016-02-20 14:25:54 +0100

    added expecta and ocmock to the template

--- Specta Spec.xctemplate/___FILEBASENAME___.m
+++ Specta Spec.xctemplate/___FILEBASENAME___.m
@@ -6,31 +6,23 @@
 //  Copyright ___YEAR___ ___ORGANIZATIONNAME___. All rights reserved.
 //
 
-#import "Specta.h"
+#import <Foundation/Foundation.h>
+
+// Tests
+#import <Specta/Specta.h>
+#define EXP_SHORTHAND
+#import <Expecta/Expecta.h>
+#import <OCMock/OCMock.h>
+
+// Custom
 #import "___VARIABLE_testedClass___.h"
 
 
 SpecBegin(___VARIABLE_testedClass___)
 
 describe(@"___VARIABLE_testedClass___", ^{
-    
-    beforeAll(^{
 
-    });
-    
-    beforeEach(^{
-
-    });
-    
     it(@"", ^{
-
-    });  
-    
-    afterEach(^{
-
-    });
-    
-    afterAll(^{
 
     });
 });
